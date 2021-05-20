73817849721db1911f3246279cdd1869398b4daa
Tyler <guerrero.tyler3@gmail.com>
2021-05-20 18:00:56 -0400

    Delete Config.js

--- config/Config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    database: '',
-    google : {
-        clientID: "9848792882-ie78ij0l4s8gqcp69ovm2fugaun76dr6.apps.googleusercontent.com",
-        clientSecret: "8t09X-6FP9c4ZGreSaf3g72i"
-    }
-}
